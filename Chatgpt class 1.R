e45fa8233cf128d0907825dd6bea2331dadc7667
Francesco Bazzana <frabaz@MacBook-Air-di-Francesco-3.local>
2026-09-16 16:55:51 +0200

    Last commit

---
+++ Chatgpt class 1.R
@@ -0,0 +1,43 @@
+2+2
+365*24
+"cat" == "dog"
+
+#OBJECTS
+
+berlin_population <- 3896700
+berlin_area <- 891.3
+
+berlin_population
+berlin_area
+berlin_population_density <- berlin_population/berlin_area
+berlin_population_density
+
+hours_in_a_day <- 24
+days_in_week <- 7
+hours_in_week <- hours_in_a_day*days_in_week
+
+#OBJECTS AND FUNCTIONS
+
+class(hours_in_a_day)
+
+country <- "Germany"
+population <- 84000000
+my_calculation <- population / 2
+class(country)
+class(population)
+class(my_calculation)
+
+#VECTORS
+
+years <- c(2020, 2021, 2022, 2023, 2024, 2025, 2026)
+years
+
+new_years <- c(2027, 2028, 2029, 2030)
+total_years <- c(years, new_years)
+total_years
+
+prices <- c(10, 20, 30, 40)
+prices * 2
+prices +5
+more_prices <- c(50,60,70)
+all_prices <- c(prices, more_prices)
