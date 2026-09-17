37b12c7812d803be226a90854a08b3076bfb1614
Francesco Bazzana <frabaz@MacBook-Air-di-Francesco-3.local>
2026-09-17 12:53:24 +0200

    second modification override

--- Lecture 3.R
+++ Lecture 3.R
@@ -7,4 +7,4 @@
 #Iteration with purrr --> allows us to do iteration in a consistent way, applyinga function to a object (that can be of any type), and then return an output that is of pre-defined type
 #map(.x, .f, ...) 
 #map(starwards, class)
-#(.x) purr style way of providing a formula ??
+#(.x) purr style way of providing a formula ??ss
