55619c545e4bb4c36a8731abe6068502cc1f7f88
Francesco Bazzana <frabaz@MacBook-Air-di-Francesco-3.local>
2026-09-17 12:52:52 +0200

    second modification

---
+++ Lecture 3.R
@@ -0,0 +1,10 @@
+#ITERATION
+
+#for() loops, |> pipe operator is the same as %>%
+
+#while() when you do not know the number of oeprations in advance 
+
+#Iteration with purrr --> allows us to do iteration in a consistent way, applyinga function to a object (that can be of any type), and then return an output that is of pre-defined type
+#map(.x, .f, ...) 
+#map(starwards, class)
+#(.x) purr style way of providing a formula ??
